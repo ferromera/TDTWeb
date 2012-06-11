b5c0a9ddfc1aecfd60b88b5c620d710bf9dc5459
Fernando Romera Ferrio <fernandoromeraferrio@gmail.com>
2012-06-11 06:17:36 -0300

    agrega accesors

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,10 @@
 class Team < ActiveRecord::Base
   attr_accessible :name
+  attr_accessor :photo_file_name
+  attr_accessor :photo_content_type
+  attr_accessor :photo_file_size
+  attr_accessor :photo_updated_at
+
   validates :name , presence: true
   belongs_to :user
   has_many :players
