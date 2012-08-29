3862ebe410a2d7cef47be316d286f27d9b61ffef
Fernando Romera Ferrio <fernandoromeraferrio@gmail.com>
2012-08-29 16:27:19 -0300

    modifica nombre de escudos

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -4,9 +4,10 @@ module TeamsHelper
       when team.name.downcase == "honolulu lights" then "/assets/teams/HL.png"
       when team.name.downcase == "bazinga f.c." then "/assets/teams/BZ.png"
       when team.name.downcase == "wisconsin badgers" then "/assets/teams/WB.png"
-      when team.name.downcase == "comando superior peronista" then "/assets/teams/CS.png"
+      when ((team.name.downcase == "comando superior peronista") or 
+        (team.name.downcase == "c. s. peronista")) then "/assets/teams/CS.png"
       when team.name.downcase == "saint bastian" then "/assets/teams/SB.png"
-      when team.name.downcase == "deportivo fiesta" then "/assets/teams/DF.png"
+      when team.name.downcase == "sportivo fiesta" then "/assets/teams/DF.png"
       when team.name.downcase == "uspallata f.b." then "/assets/teams/US.png"
       when team.name.downcase == "antigua y barbuda" then "/assets/teams/AB.png"
       end
