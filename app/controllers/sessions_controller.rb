ba4ff6b8e3e24871ff4489609d3e553719c661a3
Fernando Romera Ferrio <fernandoromeraferrio@gmail.com>
2013-03-10 22:28:44 -0300

    error al sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
  
 def create
     user = User.find_by_email(params[:session][:email])
-    current_user = user
-    rredirect_to root_path
+    
+    redirect_to root_path
     #if user && user.authenticate(params[:session][:password])
      # if ! sign_in user
      #   flash[:error] = 'No puede iniciar sesión porque su cuenta está en proceso de autorización.'
