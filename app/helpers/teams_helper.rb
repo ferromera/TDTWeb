580499e3e40762db93d59ef9b110e75a7279184e
Fernando Romera Ferrio <fernandoromeraferrio@gmail.com>
2013-03-27 15:36:27 -0300

    default emblem

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -11,6 +11,7 @@ module TeamsHelper
       when team.name.downcase == "uspallata f.b." then "/assets/teams/US.png"
       when team.name.downcase == "antigua y barbuda" then "/assets/teams/AB.png"
       when team.name.downcase == "green machine" then "/assets/teams/GM.png"
+      else "/assets/teams/default_emblem.png"
       end
   end
 end
